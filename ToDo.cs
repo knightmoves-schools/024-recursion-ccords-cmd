4de3d1cdbf2754fc155edee9a82d64a7ff71e5f7
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-30 14:24:16 -0500

    Update ToDo.cs

--- ToDo.cs
+++ ToDo.cs
@@ -1,5 +1,13 @@
 ﻿namespace knightmoves;
 public class ToDo
 {
-    // Add your code here
+    public string[] MarkAsDone(string[] todos)
+    {
+        string[] result = new string[todos.Length];
+        for (int i = 0; i < todos.Length; i++)
+        {
+            result[i] = "done - " + todos[i];
+        }
+        return result;
+    }
 }
